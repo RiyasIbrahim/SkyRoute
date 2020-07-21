db5bbac4b75cf8f0543ccea4ab6393791371a627
Riyas <riyas@student.nitw.ac.in>
2020-07-21 13:08:55 +0530

    set start point and end point

--- script.py
+++ script.py
@@ -10,12 +10,52 @@ for letter, landmark in landmark_choices.items():
 
 def greet():
     print("Hi there and welcome to SkyRoute!")
-    print("We'll help you find the shortest\
-     route between the following Vancouver\
-      landmarks:\n" + landmark_string)
+    print("We'll help you find the shortest \
+    route between the following Vancouver \
+    landmarks:\n" + landmark_string)
 
 def skyroute():
     greet()
 
+def set_start_and_end(start_point, end_point):
+    if start_point:
+        change_point = input("What would you like to change? \
+        You can enter 'o' for 'origin', 'd' for 'destination' \
+        , or 'b' for 'both': ")
+        if change_point == 'b':
+            start_point = get_start()
+            end_point = get_end()
+        elif change_point == 'o':
+            start_point = get_start()
+        elif change_point == 'd':
+            end_point = get_end()
+        else:
+            print("Oops, that isn't 'o', 'd', or 'b'...")
+            set_start_and_end(start_point, end_point)
+    else:
+        start_point = get_start()
+        end_point = get_end()
+    return start_point, end_point
 
-skyroute()
+def get_start():
+    start_point_letter = input("Where are you coming from?\
+    Type in the corresponding letter: ")
+    if landmark_choices[start_point_letter]:
+        start_point = landmark_choices[start_point_letter]
+        return start_point
+    else:
+        print("Sorry, that's not a landmark we have \
+        data on. Let's try this again...")
+        get_start()
+
+def get_end():
+    end_point_letter = input("Ok, where are you headed? \
+    Type in the corresponding letter: ")
+    if landmark_choices[end_point_letter]:
+        end_point = landmark_choices[end_point_letter]
+        return end_point
+    else:
+        print("Sorry, that's not a landmark we have \
+        data on. Let's try this again...")
+        get_end()
+print(set_start_and_end(None, None))
